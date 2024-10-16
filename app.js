9df56f2f202a74fb5ff28cf4ac33b0c076e3ed9c
Groot Dev <wavestorm42@gmail.com>
2024-10-15 15:21:09 -1000

    Use new http server required for socket.io server

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require('socket.io')
+const io = new Server(server)
 
 const port = 3000
 

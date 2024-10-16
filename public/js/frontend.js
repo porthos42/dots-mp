c6f8517d409071a517e94440ea713aa8cc31ded1
Groot Dev <wavestorm42@gmail.com>
2024-10-16 09:07:41 -1000

    Refactor variables and file names to make them easier to read.

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -11,15 +11,14 @@ canvas.height = innerHeight
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const players = {}
-const backendPlayers = {}
+const frontEndPlayers = {}
 
 socket.on('updatePlayers', (backendPlayers) => {
   for (const id in backendPlayers) {
     const backendPlayer = backendPlayers[id]
 
-    if (!players[id]) {
-      players[id] = new Player({
+    if (!frontEndPlayers[id]) {
+      frontEndPlayers[id] = new Player({
         x: backendPlayer.x, 
         y: backendPlayer.y,
         radius: 10,
@@ -27,9 +26,9 @@ socket.on('updatePlayers', (backendPlayers) => {
     }
   }
 
-  for (const id in players) {
+  for (const id in frontEndPlayers) {
     if (!backendPlayers[id]) {
-      delete players[id]
+      delete frontEndPlayers[id]
     }
   }
 
@@ -43,8 +42,8 @@ function animate() {
   c.fillStyle = 'rgba(0, 0, 0, 0.1)'
   c.fillRect(0, 0, canvas.width, canvas.height)
 
-  for (const id in players) {
-    const player = players[id]
+  for (const id in frontEndPlayers) {
+    const player = frontEndPlayers[id]
     player.draw()
   }  
 }

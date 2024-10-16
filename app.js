3520df2dac4e7a4f35c45ae45ae8886434c8cb14
Groot Dev <wavestorm42@gmail.com>
2024-10-15 15:18:48 -1000

    Create new http server.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+
+const http = require('http');
+const server = http.createServer(app);
+
 const port = 3000
 
 app.use(express.static('public'))
@@ -8,6 +12,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })

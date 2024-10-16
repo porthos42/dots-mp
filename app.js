b01c8c2921b31250fd1878083659dbd5d983621b
Groot Dev <wavestorm42@gmail.com>
2024-10-15 15:41:36 -1000

    Listen for connection events and finish creating initial connection.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+// socket.io setup
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io')
@@ -12,6 +13,10 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
 })
 
 server.listen(port, () => {
